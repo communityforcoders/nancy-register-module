ce1b51af2aaca5490ab80be1fb89bd94be5327c8
kacperduras <git@kacperduras.pl>
2018-01-04 15:57:18 +0100

    Implement configuration and welcome message.

--- src/main/java/pl.communityforcoders.register/RegisterModule.java
+++ src/main/java/pl.communityforcoders.register/RegisterModule.java
@@ -1,5 +1,6 @@
 package pl.communityforcoders.register;
 
+import java.io.File;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import pl.communityforcoders.nancy.Nancy;
@@ -7,25 +8,30 @@ import pl.communityforcoders.nancy.module.annotation.Listener;
 import pl.communityforcoders.nancy.module.annotation.Manifest;
 import pl.communityforcoders.nancy.module.annotation.OnDisable;
 import pl.communityforcoders.nancy.module.annotation.OnEnable;
+import pl.communityforcoders.nancy.util.ConfigUtils;
 
 @Manifest(name = "RegisterModule", author = "kacperduras", version = "1.0.0.0")
 public class RegisterModule {
 
   private Nancy nancy;
+  private RegisterModuleConfiguration config;
 
   @OnEnable
   public void onEnable(Nancy nancy) {
     this.nancy = nancy;
+    config = ConfigUtils.loadConfig(new File("config.json"), RegisterModuleConfiguration.class);
   }
 
   @OnDisable
   public void onDisable() {
-    this.nancy = null;
+    config = null;
+    nancy = null;
   }
 
   @Listener
   public void onJoin(GuildMemberJoinEvent event) {
-
+    event.getUser().openPrivateChannel()
+        .queue(channel -> config.getWelcomeMessage().forEach(channel::sendMessage));
   }
 
   @Listener
---
+++ src/main/java/pl.communityforcoders.register/RegisterModuleConfiguration.java
@@ -0,0 +1,14 @@
+package pl.communityforcoders.register;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RegisterModuleConfiguration {
+
+  private final List<String> welcomeMessage = Collections.emptyList();
+
+  public List<String> getWelcomeMessage() {
+    return welcomeMessage;
+  }
+
+}
